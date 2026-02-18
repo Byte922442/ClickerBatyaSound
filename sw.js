6458297b95d3f0361a60278ba603bc678293f0b4
Byte92 <strimgames5653@gmail.com>
2026-02-18 16:27:18 +0300

    Update sw.js
    Signed-off-by: Byte92 <strimgames5653@gmail.com>

--- sw.js
+++ sw.js
@@ -1,16 +1,2 @@
-const cacheName = 'batya-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './photo_7_2026-02-01_23-17-10.png',
-  './fonovaya_muzyka.mp3'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(cacheName).then((c) => c.addAll(assets)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+self.addEventListener('install', (e) => { console.log('SW Installed'); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
