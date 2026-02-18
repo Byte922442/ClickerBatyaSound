943739aea3a8204341edfd41f9bd406f1e532389
Byte92 <strimgames5653@gmail.com>
2026-02-18 16:33:36 +0300

    Update sw.js
    Signed-off-by: Byte92 <strimgames5653@gmail.com>

--- sw.js
+++ sw.js
@@ -1,2 +1,5 @@
-self.addEventListener('install', (e) => { console.log('SW Installed'); });
-self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(() => console.log("Service Worker зарегистрирован"))
+    .catch(err => console.log("Ошибка SW:", err));
+}
